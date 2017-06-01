5a74b99f6be069395fc8f91671346ad2703b9fbd
SAE <julian.dunskus@hispeed.ch>
2017-06-01 10:07:31 +0200

    Attempting to use PAG more effectively

--- src/ch/ethz/sae/Logger.java
+++ src/ch/ethz/sae/Logger.java
@@ -40,9 +40,9 @@ public class Logger {
 	private static void print(Object o) {
 		if (o instanceof Interval && ((Interval) o).isBottom()){
 			System.out.print("[ ]");
-		} else if(o instanceof Interval && ((Interval) o).isTop()){
+		} /*else if (o instanceof Interval && ((Interval) o).isTop()) {
 			System.out.print("[ TOP ]");
-		} else
+		}*/ else
 			System.out.print(o);
 	}
 }
